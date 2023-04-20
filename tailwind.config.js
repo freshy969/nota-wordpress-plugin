a007118cb90205e921d9795b7768793662ef3fe6
Matt Sanders <matt@heynota.com>
2023-04-21 11:40:11 +1200

    Updates Tailwind Prefix

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,7 @@ module.exports = {
   plugins: [],
 
   // add a prefix so we don't conflict with other WP plugins
-  prefix: 'nota-tw-',
+  prefix: 'ntw-',
   corePlugins: {
     // disable preflight so that we don't override WP styles
     preflight: false,
