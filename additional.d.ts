b06584c8e8ccf5349fa4f410829a51bc4043d55d
Matt Sanders <matt@heynota.com>
2023-05-05 15:59:16 +1200

    [TECH-5065] Adds WP Data store types

--- additional.d.ts
+++ additional.d.ts
@@ -2,5 +2,36 @@ interface Window {
   notaTools: {
     ajaxUrl: string
     nonce: string
+    components: {
+      categories: boolean
+      tags: boolean
+    }
   }
+}
+
+// many of the WordPress data stores are not typed properly
+// and for some reason redeclaring the @wordpress/data module doesn't augment
+// it only overwrites it. The easiest way is to just namespace these separately
+namespace WordPress {
+  interface CoreStoreSelectors {
+    getTaxonomy: (taxonomy: string) =>
+      | {
+          rest_base: string
+          rest_namespace?: string
+        }
+      | undefined
+  }
+
+  interface CoreEditorStoreSelectors {
+    getEditedPostContent: () => string
+    getEditedPostAttribute: <T>(attribute: string) => T
+  }
+
+  function WpDataSelectFn(store: 'core'): CoreStoreSelectors
+  function WpDataSelectFn(store: 'core/editor'): CoreEditorStoreSelectors
+
+  export type useSelect = <T>(
+    selector: (s: typeof WpDataSelectFn) => T,
+    deps?: any[],
+  ) => T
 }
