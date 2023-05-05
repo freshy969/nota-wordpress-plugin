cdebb5f545f6de854660847ea15aeaafa8d86d03
Matt Sanders <matt@heynota.com>
2023-05-05 16:00:22 +1200

    [TECH-5065] Adds WP Service

--- assets/js/services/types.ts
+++ assets/js/services/types.ts
@@ -12,3 +12,14 @@ export interface NlpService {
   getKeywords(args: KeywordsRequest): Promise<Keywords>
   getSummary(args: SummaryRequest): Promise<Summary>
 }
+
+interface FindOrCreateTermArgs {
+  term: string
+  restBase: string
+  namespace: string
+}
+export interface WordPressService {
+  findOrCreateTerm: (
+    args: FindOrCreateTermArgs,
+  ) => Promise<{ id: number; term: string }>
+}
