a06d986981eedc1f75085cff370ca61f1f67c46c
Matt Sanders <matt@heynota.com>
2023-05-10 06:13:19 +1200

    [TECH-5071] Adds warning when block editor is disabled

--- templates/admin/post-tools-meta-box.php
+++ templates/admin/post-tools-meta-box.php
@@ -7,5 +7,10 @@
 
 defined( 'ABSPATH' ) || exit;
 
+$screen = get_current_screen();
 ?>
+<?php if ( $screen->is_block_editor() ) : ?>
 <div id="nota-post-tools-meta-box-root"></div>
+<?php else : ?>
+<p>Nota currently only supports the block editor.</p>
+<?php endif; ?>
