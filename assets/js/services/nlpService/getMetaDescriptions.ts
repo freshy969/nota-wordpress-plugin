dbf403e1944e3e45ad7217f357620deb80833510
Matt Sanders <matt@heynota.com>
2023-05-09 15:03:13 +1200

    [TECH-5086][TECH-5087] Adds meta title & desc services

---
+++ assets/js/services/nlpService/getMetaDescriptions.ts
@@ -0,0 +1,25 @@
+import { NlpService } from 'assets/js/services/types'
+import fetch from 'assets/js/utils/fetch/fetch'
+import qs from 'qs'
+
+export const getMetaDescriptions: NlpService['getMetaDescriptions'] = ({
+  postHTML,
+  count,
+}) => {
+  return fetch
+    .post<{ result: { metaDescriptions: string[] } }>(
+      window.notaTools.ajaxUrl,
+      {
+        data: qs.stringify({
+          action: 'nota_action',
+          nonce: window.notaTools.nonce,
+          nota: {
+            nota_action: 'get_text_meta_descriptions',
+            postHTML,
+            count,
+          },
+        }),
+      },
+    )
+    .then(({ data }) => data.result)
+}
--- assets/js/services/nlpService/nlpService.ts
+++ assets/js/services/nlpService/nlpService.ts
@@ -2,9 +2,13 @@ import { NlpService } from 'assets/js/services/types'
 import { getHeadlines } from 'assets/js/services/nlpService/getHeadlines'
 import { getSummary } from 'assets/js/services/nlpService/getSummary'
 import { getKeywords } from 'assets/js/services/nlpService/getKeywords'
+import { getMetaDescriptions } from 'assets/js/services/nlpService/getMetaDescriptions'
+import { getMetaTitles } from 'assets/js/services/nlpService/getMetaTitles'
 
 export const nlpService: NlpService = {
   getHeadlines,
   getKeywords,
+  getMetaDescriptions,
+  getMetaTitles,
   getSummary,
 }
