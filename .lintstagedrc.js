18ae979ac61c34875f2edcec597a6c63c3e69795
Matt Sanders <matt@heynota.com>
2023-05-10 13:16:21 +1200

    [TECH-5028] Lints JS files

---
+++ .lintstagedrc.js
@@ -0,0 +1,11 @@
+const path = require('path')
+
+const buildEslintCommand = (filenames) =>
+  `yarn eslint --fix ${filenames
+    .map((f) => path.relative(process.cwd(), f))
+    .join(' ')}`
+
+module.exports = {
+  '*.{js,ts,jsx,tsx}': [buildEslintCommand],
+  '*.{js,ts,jsx,tsx,json,md}': 'prettier --write',
+}
