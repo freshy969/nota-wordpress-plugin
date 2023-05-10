12d4c20c858eaddbd272fc640a369d148ce410fc
Matt Sanders <matt@heynota.com>
2023-05-10 13:36:14 +1200

    [TECH-5028] Lints staged php files

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -8,4 +8,7 @@ const buildEslintCommand = (filenames) =>
 module.exports = {
   '*.{js,ts,jsx,tsx}': [buildEslintCommand],
   '*.{js,ts,jsx,tsx,json,md}': 'prettier --write',
+  // will lint files but will not fix them
+  // getting phpcbf to play nicely here is a bit of a pain
+  '*.php': ['./vendor/bin/phpcs -n -p'],
 }
