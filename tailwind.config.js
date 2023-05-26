8f4f139c90aa6a487e330ad49486b5c284c8d799
Matt Sanders <matt@heynota.com>
2023-05-26 13:03:14 +1200

    Adds text styles and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,67 @@
+const toRem = (px) => {
+  return `${px / 16}rem`
+}
+
 module.exports = {
   content: ['./assets/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        'h-900': [
+          toRem(28),
+          {
+            lineHeight: toRem(32),
+            fontWeight: 700,
+          },
+        ],
+        'h-800': [
+          toRem(24),
+          {
+            lineHeight: toRem(32),
+            fontWeight: 700,
+          },
+        ],
+        'paragraph-tight': [
+          toRem(16),
+          {
+            lineHeight: toRem(16),
+            fontWeight: 400,
+          },
+        ],
+        'paragraph-base': [
+          toRem(16),
+          {
+            lineHeight: toRem(24),
+            fontWeight: 500,
+          },
+        ],
+        'paragraph-large': [
+          toRem(20),
+          {
+            lineHeight: toRem(24),
+            fontWeight: 400,
+          },
+        ],
+      },
+      sizes: {
+        '8px': toRem(8),
+        '16px': toRem(16),
+      },
+    },
+    colors: {
+      button: {
+        default: '#3A7DFF',
+        hover: '#2267EC',
+        destructive: '#D83E35',
+        'destructive-hover': '#BC342C',
+      },
+      typography: {
+        dark: '#050E21',
+        medium: '#5E6064',
+        light: '#FFFFFF',
+        disabled: '#8B8B8B',
+      },
+    },
   },
   plugins: [],
 
