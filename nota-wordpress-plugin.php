6b0a2e4d07756289eabb37bff65a922c99b27068
Matt Sanders <matt@heynota.com>
2023-05-30 10:00:17 +1200

    v0.1.2

--- nota-wordpress-plugin.php
+++ nota-wordpress-plugin.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Nota
  * Description: Nota uses AI tools to optimize the publishing process.
- * Version: 0.1.1
+ * Version: 0.1.2
  * Author: Nota
  * Author URI: https://heynota.com
  * License: GPL2
@@ -13,7 +13,7 @@
 
 defined( 'ABSPATH' ) || exit;
 
-define( 'NOTA_PLUGIN_VERSION', '0.1.1' );
+define( 'NOTA_PLUGIN_VERSION', '0.1.2' );
 
 if ( ! defined( 'NOTA_PLUGIN_FILE' ) ) {
 	define( 'NOTA_PLUGIN_FILE', __FILE__ );
