18f506b2398309b9882feb7e78ff4cf0297c6af6
Matt Sanders <matt@heynota.com>
2023-06-22 14:42:11 +1200

    [TECH-5675] Displays Nota error message

--- assets/js/components/AsyncStateManager/AsyncStateManager.tsx
+++ assets/js/components/AsyncStateManager/AsyncStateManager.tsx
@@ -1,15 +1,48 @@
 import { Button } from '@wordpress/components'
 
+const defaultErrorMessage = 'Unknown error occurred.'
+const getErrorMessage = (errors: unknown) => {
+  // errors are untyped so we need to narrow them down into something we can trust
+  // it's a mouthful, but each of these is required to ensure that we have a Fetch error
+  // that conforms to our Nota Error structure
+  if (
+    errors === null ||
+    typeof errors !== 'object' ||
+    !('data' in errors) ||
+    errors.data === null ||
+    typeof errors.data !== 'object' ||
+    !('data' in errors.data)
+  )
+    return defaultErrorMessage
+
+  // WP encodes errors as such:
+  // { data: { code: string; message: string }[] }
+  // so we want to find any errors that are Nota specific
+  const errorData = errors?.data?.data || []
+  if (!Array.isArray(errorData)) return null
+  const notaError = errorData.find(
+    (error): error is { message: string } =>
+      // this ensures it's a nota_error
+      typeof error === 'object' &&
+      'code' in error &&
+      error.code === 'nota_error' &&
+      // this ensures our nota error has a message
+      'message' in error &&
+      typeof error.message === 'string',
+  )
+  return notaError?.message || defaultErrorMessage
+}
+
 interface Props {
   retry: () => void
   isLoading: boolean
-  hasError: boolean
+  error?: unknown
   children: React.ReactNode
 }
 
 export function AsyncStateManager({
   isLoading,
-  hasError,
+  error,
   retry,
   children,
 }: Props) {
@@ -19,9 +52,9 @@ export function AsyncStateManager({
         'Loading...'
       ) : (
         <div>
-          {hasError ? (
+          {!!error ? (
             <div>
-              There was an error.{' '}
+              {getErrorMessage(error)}{' '}
               <Button variant="link" onClick={retry}>
                 Retry
               </Button>
